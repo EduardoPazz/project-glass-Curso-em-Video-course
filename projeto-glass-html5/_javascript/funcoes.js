20d4079a85525460dc084fce93d0148b01f91eb2
Eduardo da Paz <jooeduardo8@gmail.com>
2020-02-24 18:07:00 -0300

    Setting the img changing feature

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,33 @@
+/* changing header img by the hover of the mouse in the nav items */
+
+const links = document.querySelectorAll('nav li')
+const img = document.querySelector('header img')
+
+links.forEach(link => {
+    let src;
+
+    switch (link.textContent) {
+        case 'Home':
+            src = './_imagens/home.png';
+            break;
+
+        case 'Especificações':
+            src = './_imagens/especificacoes.png';
+            break;
+
+        case 'Fotos':
+            src = './_imagens/fotos.png';
+            break;
+
+        case 'Multimídia':
+            src = './_imagens/multimidia.png';
+            break;            
+
+        case 'Fale conosco':
+            src = './_imagens/contato.png'
+            break;
+    }
+
+    link.addEventListener('mouseover', () => img.setAttribute('src', src))
+    link.addEventListener('mouseout', () => img.setAttribute('src', './_imagens/glass-oculos-preto-peq.png'))
+})
